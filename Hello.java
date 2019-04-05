d4ae20d58983efc315818c9e36e60e8d727d7ac4
mldn <1959821818@qq.com>
2019-04-06 00:44:07 +0800

    In Hello.java file and two statement

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String args[]){
 		System.out.println("Hello Git !!!");
+		//System.out.println("认真听课的孩子是好孩子");
+		System.out.println("不认真听课的孩子到后面站着");
 	}
 }
